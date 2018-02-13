5bef840495d81027524cdda0882115d9ca0db3c0
Annie Flaherty <anne.flaherty@case.edu>
2018-02-12 21:15:39 -0500

    Initial model setup

---
+++ models/user.js
@@ -0,0 +1,9 @@
+module.exports = function(sequelize, DataTypes) {
+    var User = sequelize.define("User", {
+        user_name: DataTypes.STRING,
+        admin: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        }
+    });
+};
