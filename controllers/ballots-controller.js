28fcbd5b6fe037d2194fb912479c0416974a4212
Annie Flaherty <anne.flaherty@case.edu>
2018-02-17 11:25:06 -0500

    Added get and post api routes

--- controllers/ballots-controller.js
+++ controllers/ballots-controller.js
@@ -9,4 +9,29 @@ var db = require('../models');
 // Routes
 module.exports = function(app) {
 	app.get('/api/ballots',(req,res)=>db.Ballot.findAll({}).then(dbBallot=>res.json(dbBallot)));
+
+	app.post('/api/ballots', function(req, res) {
+		db.Ballot.create({
+			name: req.body.name,
+            expired: req.body.expired,
+            expire_dt: req.body.expire_dt,
+
+            bet_text_1: req.body.bet_text_1,
+            bet_options_1: req.body.bet_options_1,
+            bet_answer_1: req.body.bet_answer_1,
+
+            bet_text_2: req.body.bet_text_2,
+            bet_options_2: req.body.bet_options_2,
+            bet_answer_2: req.body.bet_answer_2,
+
+            bet_text_3: req.body.bet_text_3,
+            bet_options_3: req.body.bet_options_3,
+            bet_answer_3: req.body.bet_answer_3,
+
+            winning_user_id: req.body.winning_user_id,
+            total_right: req.body.total_right
+		}).then(function(dbBallot) {
+			res.json(dbBallot);
+		});
+	});
 };
--- controllers/bets-controller.js
+++ controllers/bets-controller.js
@@ -8,5 +8,23 @@ var db = require('../models');
 
 // Routes
 module.exports = function(app) {
-	app.get('/api/bets',(req,res)=>db.Bet.findAll({}).then(dbBet=>res.json(dbBet)));
+    app.get('/api/bets', (req, res) => db.Bet.findAll({}).then(dbBet => res.json(dbBet)));
+
+    app.post('/api/bets', function(req, res) {
+        db.Bet.create({
+            user_id: req.body.user_id,
+            ballot_id: req.body.ballot_id,
+            
+            user_answer_1: req.body.user_answer_1,
+            answer_truth_1: req.body.answer_truth_1,
+
+            user_answer_2: req.body.user_answer_2,
+            answer_truth_2: req.body.answer_truth_2,
+
+            user_answer_3: req.body.user_answer_3,
+            answer_truth_3: req.body.answer_truth_3,
+        }).then(function(dbBet) {
+            res.json(dbBet);
+        });
+    });
 };
