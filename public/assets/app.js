c190bf1a973283858ad3bb9e18b3b0099fff1de0
GreenCord <steve@nevizen.com>
2018-02-23 21:45:55 -0500

    Replacing files

---
+++ public/assets/app.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $(function(){
+
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+
+        $('#submit-ballot').on('click',function(e){
+        	e.preventDefault();
+
+        	var bets = {
+        		user_id: $('#currentUser').attr('data-id'),
+        		ballot_id: $('#ballotName').attr('data-id'),
+        		user_answer_1: $('input[name=ballot_group1]:checked').val(),
+        		user_answer_2: $('input[name=ballot_group2]:checked').val(),
+        		user_answer_3: $('input[name=ballot_group3]:checked').val()
+        	};
+        	console.log('bets:',bets);
+        	$.ajax('/api/bets',{
+        		type: 'POST',
+        		data: bets
+        	}).done(function(results){
+        		console.log('Bets recorded!');
+        		location = '/ballot-submitted';
+        	});
+        });
+     });
+});
